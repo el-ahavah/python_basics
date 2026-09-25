e996b0a7f3f9cf1df7db11975850e514964f9c64
el-ahavah <ihunanyajoyce4@gmail.com>
2026-09-25 17:03:17 +0100

    recoding solutions

---
+++ recoding_1.py
@@ -0,0 +1,6 @@
+def ticket_total(price, quantity):
+    total = price * quantity
+    print(total)
+    amount = ticket_total("7", 3)
+    print(amount) 
+Q1 Part A Before running the code, write the exact two output lines and state the value and type of amount. [4 marks]
